4a79bb23d90953f3c0cf93e21a3236dd14919d78
erickvneri <erickv@bluetrailsoft.com>
2020-05-29 23:51:29 -0500

    smartapp base

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+require('dotenv').config();
+
+// Main imports
+const express = require('express');
+const axios = require('axios');
+const SmartApp = require('@smartthings/smartapp');
+
+// Sms notification service import
+const { smsNotification } = require('./services');
+
+// Server init
+const server = express();
+server.use(express.json())
+// SmartApp init
+const smartApp = new SmartApp()
+
+// SmartApp definition
+smartApp.appId(process.env.APP_ID)
+        .enableEventLogging(2)
+        .permissions(['r:devices:*', 'x:devices:*', 'r:rules:*', 'w:rules:*', 'r:locations:*'])
+        .page('mainPage', (ctx, page, configData) => {
+            page.section('devices', section => {
+                section.deviceSetting('batteryCheck')
+                       .name('Select your devices')
+                       .description('')
+                       .capabilities(['battery'])
+                       .required(true)
+                       .multiple(true)
+                       .permissions('rx')
+            })
+        })
+        .updated(async (context, updateData) => {
+            // INSTALL LIFECYCLE
+        })
+
+// Enable server routing
+server.post('/battery-check', (req, res) => {
+    smartApp.handleHttpCallback(req, res)
+})
+
+// Server listener
+server.listen(process.env.PORT, () => {
+    console.log('Server listening at port ', process.env.PORT)
+})
